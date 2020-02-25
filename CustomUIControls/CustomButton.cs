47b0510e4d03715946d93efd7e81262bcdd31ff2
Haagimus <Haagimus@gmail.com>
2020-02-24 21:25:38 -0600

    initial commit

---
+++ CustomUIControls/CustomButton.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CustomUIControls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomUIControls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomUIControls;assembly=CustomUIControls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:CustomButton/>
+    ///
+    /// </summary>
+
+    public enum ColorKey
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Gray
+    }
+
+    public class CustomButton : Control
+    {
+        static CustomButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(CustomButton), 
+                new FrameworkPropertyMetadata(typeof(CustomButton)));
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(
+                "Text",
+                typeof(string),
+                typeof(CustomButton),
+                new UIPropertyMetadata(null));
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty ColorKeyProperty =
+            DependencyProperty.Register(
+                "ButtonColor",
+                typeof(ColorKey),
+                typeof(CustomButton),
+                new FrameworkPropertyMetadata(ColorKey.Gray));
+
+        public ColorKey ButtonColor
+        {
+            get { return (ColorKey)GetValue(ColorKeyProperty); }
+            set { SetValue(ColorKeyProperty, value); }
+        }
+    }
+}
---
+++ CustomUIControls/SearchTextBox.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace CustomUIControls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomUIControls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CustomUIControls;assembly=CustomUIControls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:SearchTextBox/>
+    ///
+    /// </summary>
+
+    public enum SearchMode
+    {
+        Instant,
+        Delayed
+    }
+
+    public class SearchTextBox : TextBox
+    {
+        private DispatcherTimer searchEventDelayTimer;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            Border iconBorder = GetTemplateChild("PART_SearchIconBorder") as Border;
+            if (iconBorder != null)
+            {
+                iconBorder.MouseLeftButtonDown += new MouseButtonEventHandler(IconBorder_MouseLeftButtonDown);
+                iconBorder.MouseLeftButtonUp += new MouseButtonEventHandler(IconBorder_MouseLeftButtonUp);
+                iconBorder.MouseLeave += new MouseEventHandler(IconBorder_MouseLeave);
+            }
+        }
+
+        public SearchTextBox() : base()
+        {
+            searchEventDelayTimer = new DispatcherTimer();
+            searchEventDelayTimer.Interval = SearchEventTimeDelay.TimeSpan;
+            searchEventDelayTimer.Tick += new EventHandler(OnSearchEventDelayTimerTick);
+        }
+
+        void OnSearchEventDelayTimerTick(object o, EventArgs e)
+        {
+            searchEventDelayTimer.Stop();
+            RaiseSearchEvent();
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            HasText = Text.Length != 0;
+
+            if(SearchMode == SearchMode.Instant)
+            {
+                searchEventDelayTimer.Stop();
+                searchEventDelayTimer.Start();
+            }
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if(e.Key == Key.Escape && SearchMode == SearchMode.Instant)
+            {
+                this.Text = "";
+            }
+            else if ((e.Key == Key.Return || e.Key == Key.Enter) && SearchMode == SearchMode.Delayed)
+            {
+                RaiseSearchEvent();
+            }
+            else
+            {
+                base.OnKeyDown(e);
+            }
+        }
+
+        static void OnSearchEventTimeDelayChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            SearchTextBox stb = o as SearchTextBox;
+            if (stb != null)
+            {
+                stb.searchEventDelayTimer.Interval = ((Duration)e.NewValue).TimeSpan;
+                stb.searchEventDelayTimer.Stop();
+            }
+        }
+
+        public static readonly RoutedEvent SearchEvent =
+            EventManager.RegisterRoutedEvent(
+                "Search",
+                RoutingStrategy.Bubble,
+                typeof(RoutedEventHandler),
+                typeof(SearchTextBox));
+
+        public event RoutedEventHandler Search {
+            add { AddHandler(SearchEvent, value); }
+            remove { RemoveHandler(SearchEvent, value); }
+        }
+
+        public static DependencyProperty LabelTextProperty =
+            DependencyProperty.Register(
+                "LabelText",
+                typeof(string),
+                typeof(SearchTextBox));
+
+        public static DependencyProperty LabelTextColorProperty =
+            DependencyProperty.Register(
+                "LabelTextColor",
+                typeof(Brush),
+                typeof(SearchTextBox));
+
+        public static DependencyProperty SearchModeProperty =
+            DependencyProperty.Register(
+                "SearchMode",
+                typeof(SearchMode),
+                typeof(SearchTextBox),
+                new PropertyMetadata(SearchMode.Instant));
+
+        public static DependencyPropertyKey HasTextPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+                "HasText",
+                typeof(bool),
+                typeof(SearchTextBox),
+                new PropertyMetadata());
+
+        public static DependencyProperty HasTextProperty =
+            HasTextPropertyKey.DependencyProperty;
+
+        private static DependencyPropertyKey IsMouseLeftButtonDownPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+            "IsMouseLeftButtonDown",
+            typeof(bool),
+            typeof(SearchTextBox),
+            new PropertyMetadata());
+
+        public static DependencyProperty IsMouseLeftButtonDownProperty =
+            IsMouseLeftButtonDownPropertyKey.DependencyProperty;
+
+        public static DependencyProperty SearchEventTimeDelayProperty =
+            DependencyProperty.Register(
+                "SearchEventTimeDelay",
+                typeof(Duration),
+                typeof(SearchTextBox),
+                new FrameworkPropertyMetadata(
+                    new Duration(new TimeSpan(0, 0, 0, 0, 500)),
+                    new PropertyChangedCallback(OnSearchEventTimeDelayChanged)));
+
+        private void IconBorder_MouseLeftButtonDown(object obj, MouseButtonEventArgs e)
+        {
+            IsMouseLeftButtonDown = true;
+        }
+
+        private void IconBorder_MouseLeftButtonUp(object obj, MouseButtonEventArgs e)
+        {
+            if (!IsMouseLeftButtonDown) return;
+
+            if (HasText && SearchMode == SearchMode.Instant)
+            {
+                this.Text = "";
+            }
+            if (HasText && SearchMode == SearchMode.Delayed)
+            {
+                RaiseSearchEvent();
+            }
+
+            IsMouseLeftButtonDown = false;
+        }
+
+        private void IconBorder_MouseLeave(object obj, MouseEventArgs e)
+        {
+
+            IsMouseLeftButtonDown = false;
+        }
+
+        public void RaiseSearchEvent()
+        {
+            RoutedEventArgs args = new RoutedEventArgs(SearchEvent);
+            RaiseEvent(args);
+        }
+
+        static SearchTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(SearchTextBox),
+                new FrameworkPropertyMetadata(typeof(SearchTextBox)));
+        }
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        public Brush LabelTextColor
+        {
+            get { return (Brush)GetValue(LabelTextColorProperty); }
+            set { SetValue(LabelTextColorProperty, value); }
+        }
+
+        public SearchMode SearchMode
+        {
+            get { return (SearchMode)GetValue(SearchModeProperty); }
+            set { SetValue(SearchModeProperty, value); }
+        }
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            set { SetValue(HasTextPropertyKey, value); }
+        }
+
+        public bool IsMouseLeftButtonDown
+        {
+            get { return (bool)GetValue(IsMouseLeftButtonDownProperty); }
+            private set { SetValue(IsMouseLeftButtonDownPropertyKey, value); }
+        }
+
+        public Duration SearchEventTimeDelay
+        {
+            get { return (Duration)GetValue(SearchEventTimeDelayProperty); }
+            set { SetValue(SearchEventTimeDelayProperty, value); }
+        }
+    }
+}
